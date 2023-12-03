be6bba35e3493374017bb90004317a6a5fcb1ee2
kat0d1 <60965269+CPTshkarpetka@users.noreply.github.com>
2023-12-03 23:22:04 +0200

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const tokenTG = process.env.TOKEN;
 
 var bot = new TelegramBot(tokenTG, {webHook:{
   host:"0.0.0.0",
-  port: process.env.PORT || 5000
+  port: process.env.PORT || 8080
 }});
 
 bot.setWebHook(process.env.URL);
